7d21f57fb08cdad725686c58726b498f8ae737bc
balukommagoni <48942642+balukommagoni@users.noreply.github.com>
2019-12-05 08:25:18 -0800

    Add files via upload

---
+++ notification.js
@@ -0,0 +1,13 @@
+'use strict';
+angular.module('myApp.notification', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/notification', {
+    templateUrl: 'notification/notification.html',
+    controller: 'notificationCtrl'
+  });
+}])
+.controller('notificationCtrl', ['$scope','dispFactory',function($scope,dispFactory) {
+  dispFactory.getdata().then(function (data) {
+    $scope.CompleteInfo=data.data;
+})
+}]);
