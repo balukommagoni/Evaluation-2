6c4edb7759de9593b80062e717da4aa173cbc26e
balukommagoni <48942642+balukommagoni@users.noreply.github.com>
2019-12-09 07:00:32 -0800

    Update home.js

--- home.js
+++ home.js
@@ -5,10 +5,21 @@ angular.module('myApp.home', ['ngRoute'])
     templateUrl: 'home/home.html',
     controller: 'homeCtrl'
   });
-}])
-.controller('homeCtrl', ['$scope','dispFactory',function($scope,dispFactory) {
-  dispFactory.getdata().then(function (data) {
+}]).controller('homeCtrl', ['$scope','dispFactory',function($scope,dispFactory) {
+  dispFactory.getuserdata().then(function (data) {
     $scope.CompleteData=data.data;
 })
-}]);
+}])
+.directive(directive1,function(){
+
+})
+.directive(directive2,function(){
+
+})
+.directive(directive3,function(){
+
+})
+.directive(directive4,function(){
+
+});
 
