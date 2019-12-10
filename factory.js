26ed654b08d0c0668a10af2495ffa5b6b5df471d
balukommagoni <48942642+balukommagoni@users.noreply.github.com>
2019-12-10 05:10:24 -0800

    Update factory.js

--- factory.js
+++ factory.js
@@ -1,9 +1,33 @@
 var app=angular.module('myApp.dispFactory',[]);
 app.factory('dispFactory', ['$http', function($http) {
     var dataFactory = {};
-     dataFactory.getdata = function () {
-               return $http.get(' http://demo1124924.mockable.io/getuserdetails');
+     dataFactory.getuserdata = function () {
+               return $http.get('http://demo1124924.mockable.io/getuserdetails').then(
+                function Success (response) {
+                    console.info('Resolved');
+                    return response;
+                },
+                function Failure (response) {
+                    console.error('Rejected');
+                    return response;
+                }
+            );
          };
+         dataFactory.getnotifdata = function () {
+            return $http({method: 'POST',  
+            url: "http://demo1124924.mockable.io/getNotifications",  
+            headers: { 'Ukey': 12 }
+        }).then(
+            function Success (response) {
+                console.info('Resolved');
+                return response;
+            },
+            function Failure (response) {
+                console.error('Rejected');
+                return response;
+            }
+        );
+      };
      return dataFactory;
  }]);
 
