eeca82391708412be982cd7e22b8e26aba8b0f67
balukommagoni <balasai.kommagoni@imaginea.com>
2020-01-10 15:30:22 -0800

    pagination commit

--- downloads/angular-seed-master/angular-seed-master/app/home/home.js
+++ downloads/angular-seed-master/angular-seed-master/app/home/home.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('myApp.home', ['ngRoute'])
+angular.module('myApp.home', ['ngRoute','ui.bootstrap'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {
     templateUrl: './home/home.html',
@@ -16,7 +16,7 @@ angular.module('myApp.home', ['ngRoute'])
   $scope.checkboxValue2='Query';
   $scope.checkboxValue3='Completed';
 
-  var gridData = [
+  $scope.gridData = [
     {requestID: '702', taskID: '454', branch: 'ATL', emailSubject: 'New Request Creation', transactionType:'PR',recievedOn:'09/27/19 05:22:52',loadingDate:'12/24/19 04:32:50',dueDate:'12/25/19 55:26:53',taskSubStatus:'Sorting-Assigned',assignedTo:'stgtestuser11'},
     {requestID: '703', taskID: '244', branch: 'BOS', emailSubject: 'Test', transactionType:'-',recievedOn:'09/29/19 03:21:02',loadingDate:'05/25/19 01:12:51',dueDate:'02/24/19 03:32:22',taskSubStatus:'Sorting-UnAssigned',assignedTo:'-'},
     {requestID: '749', taskID: '678', branch: 'BOI', emailSubject: 'New Request Creation', transactionType:'-',recievedOn:'08/26/19 09:14:51',loadingDate:'03/14/19 25:42:52',dueDate:'04/25/19 42:22:25',taskSubStatus:'SME_Assigned',assignedTo:'stgtestuser11'},
@@ -24,6 +24,11 @@ angular.module('myApp.home', ['ngRoute'])
     {requestID: '732', taskID: '765', branch: 'FTL', emailSubject: 'Test', transactionType:'-',recievedOn:'06/25/19 06:24:54',loadingDate:'05/23/19 05:32:51',dueDate:'03/24/19 45:24:55',taskSubStatus:'Sorting-UnAssigned',assignedTo:'stgtestuser11'},
     {requestID: '778', taskID: '864', branch: 'FLO', emailSubject: 'New Request Creation', transactionType:'-',recievedOn:'01/16/19 03:22:52',loadingDate:'03/14/19 22:42:51',dueDate:'04/25/19 42:34:56',taskSubStatus:'SME_Assigned',assignedTo:'-'}
   ];
+
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = 2;
+    $scope.maxSize = 2;
+    $scope.totalItems = 6;
 
   var expanded = false;
   $scope.showCheckboxes = function(){
@@ -43,22 +48,6 @@ angular.module('myApp.home', ['ngRoute'])
   $scope.reset=function(){
     
   }
-
-$scope.curPage = 1,
-$scope.itemsPerPage = 3,
-$scope.maxSize = 5;
-
-$scope.numOfPages = function () {
-  return Math.ceil(gridData.length / $scope.itemsPerPage);
-};
-
-  $scope.$watch('curPage + numPerPage', function() {
-  var begin = (($scope.curPage - 1) * $scope.itemsPerPage);
-  var end = begin + $scope.itemsPerPage;
-
-  $scope.gridInfo = gridData.slice(begin, end);
-  
-});
 }]).directive('directive1',function(){
   return{
     scope:{
@@ -81,7 +70,7 @@ $scope.numOfPages = function () {
 }).directive('directive3',function(){
   return{
     scope: {
-      item: '=select'
+      Data: '=select'
   }, 
       restrict:'E',
   }
